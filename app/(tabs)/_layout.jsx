cdb863ab586c3860173d2c699701219cb4907dfd
K-n4it <125224021+K-n4it@users.noreply.github.com>
2024-11-13 10:43:00 -0400

    first commit

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,12 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { View, Text } from "react-native";
+
+export default function TabLayout() {
+
+  return (
+    <View>
+      <Text>Tabs Screen</Text>
+    </View>
+  );
+}
---
+++ app/index.jsx
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native";
+
+import Tabs from "../app/(tabs)/_layout";
+
+export default function App() {
+  return <Tabs />;
+};
